6209c70f6f6a2b5b00cd21037712a31aec107ee5
ThanhNT88 <120631723+ntthanh13@users.noreply.github.com>
2026-09-10 22:10:44 +0700

    Update comment

--- bai-tap-03/ex2.js
+++ bai-tap-03/ex2.js
@@ -1,4 +1,4 @@
-const chieuCao = 172;                                 // Chiều cao 1m72
+const chieuCao = 172;                                 // Chiều cao 1m72 = 172cm
 const canNangLyTuong = (chieuCao % 100) * 9 / 10;     // Cân nặng lý tưởng = Số lẻ của chiều cao (tính bằng cm) x 9 rồi chia 10
 const canNangToiDa = chieuCao % 100;                  // Mức cân tối đa = Bằng số lẻ của chiều cao (tính bằng cm)
 const canNangToiThieu = (chieuCao % 100) * 8 / 10;    // Mức cân tối thiểu = Số lẻ của chiều cao (tính bằng cm) x 8 rồi chia 10
